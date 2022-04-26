bca02e1811d7af012663f6a803ed160e09d9eb74
Michael Carr <xyeres@me.com>
2022-04-26 15:45:13 -0700

    Add tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,9 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
-  return <h1>Hello</h1>
+  return <h1 className='text-3xl font-bold border-dashed border-4 bg-blue-200 p-3'>
+    Hello there!
+  </h1>
 }
 
 export default Home
