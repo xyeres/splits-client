0eb7afc0234060d2c10d7e79b90389c1daeb0ad4
Michael Carr <xyeres@me.com>
2022-04-29 09:36:56 -0700

    Add z-index and lint

--- components/Navbar/NavBarIcon.tsx
+++ components/Navbar/NavBarIcon.tsx
@@ -1,19 +1,19 @@
-import Link from 'next/link'
-import React, { ReactNode } from 'react'
+import Link from "next/link";
+import React, { ReactNode } from "react";
 
-const NavBarIcon = ({ icon, text, href = '#' }: { icon: ReactNode, text: string, href: string }) => {
-    return (
-        <Link href={href}>
-            <a className='navbar-icon group'>
-                {icon}
-                {text && (
-                    <span className='navbar-tooltip group-hover:scale-100'>
-                        {text}
-                    </span>
-                )}
-            </a>
-        </Link>
-    )
-}
+const NavBarIcon = ({ icon, text, href = "#" }: { icon: ReactNode, text: string, href: string }) => {
+  return (
+    <Link href={href}>
+      <a className='navbar-icon group'>
+        {icon}
+        {text && (
+          <span className='navbar-tooltip group-hover:scale-100 z-50'>
+            {text}
+          </span>
+        )}
+      </a>
+    </Link>
+  );
+};
 
-export default NavBarIcon
+export default NavBarIcon;
