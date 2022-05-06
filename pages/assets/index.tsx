b2c8dd1401cab2243104a693ee267197b0239265
Michael Carr <xyeres@me.com>
2022-05-06 14:32:35 -0700

    Add link for quick access

--- pages/assets/index.tsx
+++ pages/assets/index.tsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 type Props = {}
 
-function assets({}: Props) {
+function assets({ }: Props) {
   return (
-    <div>assets</div>
-  )
+    <div className="m-10">
+      <Link href="/assets/213424">
+        <a className="underline">
+          View an asset
+        </a>
+      </Link>
+    </div>
+  );
 }
 
-export default assets
+export default assets;
